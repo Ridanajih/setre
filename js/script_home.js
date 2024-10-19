9211db4812b97b0cca89bea7ed5cdd2716a3918e
Ridanajih <77551305+Ridanajih@users.noreply.github.com>
2024-10-19 11:19:43 +0100

    Add files via upload

---
+++ js/script_home.js
@@ -0,0 +1,17 @@
+function openTab(event, tabName) {
+    
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(content => {
+        content.classList.remove('active');
+    });
+
+    const tabButtons = document.querySelectorAll('.tab-button');
+    tabButtons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabName).classList.add('active');
+
+   
+    event.currentTarget.classList.add('active');
+}
